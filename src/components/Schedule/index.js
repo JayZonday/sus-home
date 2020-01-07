8fa597c86515655a3345a94467322fb5a211a5b0
JayZonday <jmoloughney25@gmail.com>
2020-01-06 23:53:30 -0500

    removed elvis wrong project

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -10,9 +10,6 @@ class Schedule extends Component {
     return (
       <div className='schedule-slab'>
 
-        <img className='elvis-sand' src='https://images2.minutemediacdn.com/image/upload/c_crop,h_1944,w_3456,x_0,y_360/f_auto,q_auto,w_1100/v1554910869/shape/mentalfloss/539787-istock-471070741.jpg' />
-        <div className='elvis-title'>Peanut Butter Banana Bacon Parfait</div>
-
       </div>
     );
   }
