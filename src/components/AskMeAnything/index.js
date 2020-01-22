a53baacbde9a35bdd5037bfab8912817b0ebaa14
JayZonday <jmoloughney25@gmail.com>
2020-01-22 09:32:50 -0500

    ask me section completed with dynamic body and subject filled out when hit send

--- src/components/AskMeAnything/index.js
+++ src/components/AskMeAnything/index.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import Mailto from 'react-protected-mailto'
 import Sunflower from './sf-corner-bg.png'
 import './index.css';
 
 
 class AskMeAnything extends Component {
+
+  state = {
+    question: '',
+    subject: ''
+  }
+
+  handleQuestion = (event)  => {
+
+    this.setState({
+      question: event.target.value
+    })
+  }
+
+  handleSubject = (event)  => {
+
+    this.setState({
+      subject: event.target.value
+    })
+  }
 
 
   render(){
@@ -11,9 +31,9 @@ class AskMeAnything extends Component {
     return (
       <div className='ask-slab'>
         <div className='title-slab'><h1 className='ask-title'>Ask Me Anything</h1></div>
-        <input  placeholder='Subject' onChange={()=>this.handleSubject()} className='subject'></input>
-        <input placeholder='Question'onChange={()=>this.handleQuestion()}  className='question'></input>
-        <a href='mailto:susloquet@gmail.com?Subject=Anon%question' target='_blank'><button className='send-btn'>Send</button></a>
+        <input  placeholder='Subject' onChange={this.handleSubject} className='subject'></input>
+        <input placeholder='Question'onChange={this.handleQuestion}  className='question'></input>
+        <a href={"mailto:susloquet@gmail.com?Subject=" + this.state.subject + "&body=" + this.state.question} target='_blank'><button className='send-btn'>Send</button></a>
         <img className='sunflower-design'src={Sunflower}/>
       </div>
     );
